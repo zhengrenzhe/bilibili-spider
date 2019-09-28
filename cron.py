a13e777f2d8e27ebce236b5b573fffe0d9721a01
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2019-09-28 22:54:06 +0800

    update cron config

--- cron.py
+++ cron.py
@@ -26,5 +26,6 @@ schedule.every().days.at("03:00").do(fetch_last_7_days_videos)
 schedule.every().days.at("00:01").do(daily_fetch_pagers)
 
 while True:
+    log.info(log.TARGET_CRON, "Cron Jobs check")
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(60)
