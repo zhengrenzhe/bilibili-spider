33956aa6c7a3a1a16b9752798876e5a1816d5930
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2019-10-02 00:19:37 +0800

    update

--- worker_set_visited.py
+++ worker_set_visited.py
@@ -1,5 +1,9 @@
-from infrastructure import postgres, redis
+from infrastructure import postgres, redis, log
+
+log.info(log.TARGET_REDIS, "Set visited videos list")
 
 vids = postgres.video.get_all_vids()
 for v in vids:
     redis.Context.visit(str(v[0]))
+
+log.info(log.TARGET_REDIS, "Set visited videos list finished")
